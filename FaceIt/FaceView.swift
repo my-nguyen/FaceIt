5be19b54391ea6da8868c626885f976d01c56d9a
My Nguyen <My@mys-macbook-pro.local>
2016-08-22 23:07:43 -0700

    drew the 2 eyes

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,18 +10,60 @@ import UIKit
 
 class FaceView: UIView {
 
+    // these ratios are pre-calculated by the professor
+    private struct Ratios {
+        static let SkullRadiusToEyeOffset: CGFloat = 3
+        static let SkullRadiusToEyeRadius: CGFloat = 10
+        static let SkullRadiusToMouthWidth: CGFloat = 1
+        static let SkullRadiusToMouthHeight: CGFloat = 3
+        static let SkullRadiusToMouthOffset: CGFloat = 3
+    }
+    private enum Eye {
+        case Left
+        case Right
+    }
+
+    var scale: CGFloat = 0.90
+    // can't use rect since it's just an optimization
+    // can't use frame since it's the rectangle that contains this FaceView in the superview coordinates
+    // must use bounds, but can't access bounds property in the initialization, so must change radius
+    // property to a computed property
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
     override func drawRect(rect: CGRect) {
-        // can't use rect since it's just an optimization
-        // can't use frame since it's the rectangle that contains this FaceView in the superview coordinates
-        // must use bounds
-        let radius = min(bounds.size.width, bounds.size.height) / 2
-        // var centre = convertPoint(center, fromView: superview)
-        let centre = CGPoint(x: bounds.midX, y: bounds.midY)
-        let skull = UIBezierPath(arcCenter: centre, radius: radius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: true)
-        skull.lineWidth = 5.0
         // UIColor.set() set both fill and stroke
         UIColor.blueColor().set()
-        // the actual draw
+        let skull = pathForCircle(skullCenter, withRadius: skullRadius)
         skull.stroke()
+        pathForEye(.Left).stroke()
+        pathForEye(.Right).stroke()
+    }
+
+    private func pathForCircle(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: midPoint, radius: radius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = 5.0
+        return path
+    }
+
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let radius = skullRadius / Ratios.SkullRadiusToEyeRadius
+        let centre = getEyeCenter(eye)
+        return pathForCircle(centre, withRadius: radius)
+    }
+
+    private func getEyeCenter(eye: Eye) -> CGPoint {
+        var offset = skullRadius / Ratios.SkullRadiusToEyeOffset
+        var center = skullCenter
+        center.y -= offset
+        switch eye {
+        case .Left: center.x -= offset
+        case .Right: center.x += offset
+        }
+        return center
     }
 }
