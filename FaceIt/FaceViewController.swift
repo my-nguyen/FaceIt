3c56987b22791ae26491bb0ee4c72f6a3fde76af
My Nguyen <My@mys-macbook-pro.local>
2016-08-23 17:44:47 -0700

    updated UI when there's facial expression changes in the code

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class FaceViewController: UIViewController {
-}
+    // this is the expression upon initialization of FaceView
+    var expression = FacialExpression(eyes: .Closed, eyeBrows: .Relaxed, mouth: .Smirk) {
+        didSet {
+            updateUI()
+        }
+    }
+    // this is the post-initialization UI for any change in the facial expression (eyes, eyebrows, or mouth)
+    @IBOutlet weak var faceView: FaceView! { didSet { updateUI() } }
+    private var mouthCurvatures = [FacialExpression.Mouth.Frown: -1.0, .Grin: 0.5, .Smile: 1.0, .Smirk:-0.5, .Neutral: 0.0]
+    private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed: 0.5, .Furrowed: -0.5, .Normal: 0.0]
 
+    private func updateUI() {
+        switch expression.eyes {
+        case .Open: faceView.eyesOpen = true
+        case .Closed: faceView.eyesOpen = false
+        case .Squinting: faceView.eyesOpen = false
+        }
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+    }
+}
